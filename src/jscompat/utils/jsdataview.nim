1459c7d1203b5c39f94b3edb5ec91fc81f80b8c2
litlighilit <litlighilit@foxmail.com>
2026-09-08 19:18:25 +0800

    feat(utils): DataView

---
+++ src/jscompat/utils/jsdataview.nim
@@ -0,0 +1,41 @@
+
+import std/jsffi
+import ./private/jsffiMacros
+import ./jsarraybuffer
+type
+  DataView*[T: ArrayBuffer|SharedArrayBuffer]{.importjs.} = distinct JsObject
+
+using self: DataView
+
+proc buffer*[T](self: DataView[T]): T {.importjs: "#.buffer".}
+genAttr byteLength, cint
+genAttr byteOffset, cint
+
+func len*(self): int = self.byteLength.int
+
+genNew DataView[T](buffer: T, byteOffset = cint 0, byteLength = buffer.byteLength)
+
+const defLittleEndian* = false  # either is ok, we currently use bigEndian as in network such is used
+template genGS(g, s, T, n){.dirty.} =
+  bind DataView
+  proc g*(d: DataView; i: int; isLittleEndian = defLittleEndian): T{.importcpp.}
+  proc s*(d: DataView; i: int; v: T; isLittleEndian = defLittleEndian){.importcpp.}
+
+template genIGS(n){.dirty.} =
+  genGS `getUint n`, `setUint n`, `uint n`, n
+  genGS `getInt n`, `setInt n`, `int n`, n
+template genBigIGS(n) {.dirty.} =
+  genGS `getBigUint n`, `setBigUint n`, `uint n`, n
+  genGS `getBigInt n`, `setBigInt n`, `int n`, n
+template genFGS(n){.dirty.} =
+  genGS `getFloat n`, `setFloat n`, `float n`, n
+
+genIGS 8
+genIGS 16
+genIGS 32
+genBigIGS 64
+
+#genFGS 16
+genFGS 32
+genFGS 64
+
